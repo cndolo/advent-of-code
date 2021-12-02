3b114685861afe9ad4eff9ca8bb9c8df688d5e5f
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2021-12-02 16:24:14 +0100

    Solution to part two

--- 2021/day2/src/dive.rs
+++ 2021/day2/src/dive.rs
@@ -1,6 +1,11 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn read_input_from_file(file_path: &str) -> BufReader<File> {
+    let file = File::open(file_path).expect("file not found");
+    BufReader::new(file)
+}
+
 // Returns 0 if the instruction is along the x axis and 1 otherwise
 fn parse_input(line: Vec<&str>) -> (bool, i64) {
     let unit = line[1].parse::<i64>().unwrap();
@@ -12,11 +17,32 @@ fn parse_input(line: Vec<&str>) -> (bool, i64) {
     }
 }
 
-// Reads the input and returns two vectors - one for each each direction
-fn read_input_from_file(file_path: &str) -> (Vec<i64>, Vec<i64>) {
-    let file = File::open(file_path).expect("file not found");
-    let reader = BufReader::new(file);
+// down X increases your aim by X units.
+fn apply_down(down: i64, curr_aim: i64) -> i64 {
+    curr_aim + down
+}
 
+// up X decreases your aim by X units.
+fn apply_up(up: i64, curr_aim: i64) -> i64 {
+    curr_aim - up
+}
+
+// forward X does two things:
+//  - It increases your horizontal position by X units.
+//  - It increases your depth by your aim multiplied by X.
+fn apply_forward(
+    forward: i64,
+    curr_horizontal: i64,
+    curr_aim: i64,
+    curr_vertical: i64,
+) -> (i64, i64) {
+    let horizontal = forward + curr_horizontal;
+    let depth = curr_vertical + forward * curr_aim;
+    (horizontal, depth)
+}
+// Reads the input and returns two vectors - one for each each direction
+fn compute_part_one(file_path: &str) -> (Vec<i64>, Vec<i64>) {
+    let reader = read_input_from_file(file_path);
     let mut horizontal: Vec<i64> = vec![];
     let mut vertical: Vec<i64> = vec![];
     for line in reader.lines() {
@@ -32,9 +58,36 @@ fn read_input_from_file(file_path: &str) -> (Vec<i64>, Vec<i64>) {
     (horizontal, vertical)
 }
 
+// Reads and parses input.
+// Returns the horizontal and vertical position as a tuple
+fn compute_part_two(file_path: &str) -> (i64, i64) {
+    let reader = read_input_from_file(file_path);
+    let mut aim = 0;
+    let mut curr_horizontal = 0;
+    let mut curr_vertical = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let instructions: Vec<&str> = line.split_whitespace().collect();
+        let unit = instructions[1].parse::<i64>().unwrap();
+        match instructions[0] {
+            "forward" => {
+                let new_position = apply_forward(unit, curr_horizontal, aim, curr_vertical);
+                curr_horizontal = new_position.0;
+                curr_vertical = new_position.1;
+            }
+            "down" => aim = apply_down(unit, aim),
+            "up" => aim = apply_up(unit, aim),
+            _ => panic!("unknown"),
+        }
+    }
+    (curr_horizontal, curr_vertical)
+}
+
 fn main() {
-    let (horizontal, vertical) = read_input_from_file("input.txt");
+    let (horizontal, vertical) = compute_part_one("input.txt");
     let horiz_pos: i64 = horizontal.iter().sum();
     let vert_pos: i64 = vertical.iter().sum();
-    println!("Final position: {}", horiz_pos * vert_pos);
+    println!("Part 1 final position: {}", horiz_pos * vert_pos);
+    let (horizontal, vertical) = compute_part_two("input.txt");
+    println!("Part 2 final position: {}", horizontal * vertical);
 }
